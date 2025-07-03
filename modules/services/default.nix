1869af3aaca9f892dccbfc92d69d2f5f2568de75
AramMarkarov <a.markarov@proton.me>
2025-07-03 02:11:15 +0200

    2 July + tlp changes

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -27,14 +27,14 @@
         tlp = {
             enable = true;
             settings = {
-                CPU_SCALING_GOVERNOR_ON_AC = "performance";
+                CPU_SCALING_GOVERNOR_ON_AC = "balanced";
                 CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
                 CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
                 CPU_ENERGY_PERF_POLICY_ON_AC = "default";
 
                 CPU_MIN_PERF_ON_AC = 0;
-                CPU_MAX_PERF_ON_AC = 90;
+                CPU_MAX_PERF_ON_AC = 95;
                 CPU_MIN_PERF_ON_BAT = 0;
                 CPU_MAX_PERF_ON_BAT = 85;
 
