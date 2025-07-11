3d9f9f343e3b823a63db26691acf42fbe7b08476
Aram Markarov <a.markarov@proton.me>
2025-07-11 19:56:21 +0200

    11 july

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -54,7 +54,7 @@
         pavucontrol gparted wezterm btop-rocm arduino-cli arduino-ide
         kdePackages.ark easyeffects protonup-ng mullvad-vpn yazi
         wine-staging nwg-look colord gamemode networkmanagerapplet alsa-utils
-
+        openrgb-with-all-plugins
 
     # ZSH stuff
         oh-my-posh fzf fzf-zsh
